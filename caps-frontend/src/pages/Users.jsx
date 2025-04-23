1fc13dbe2432c02aa53da93313b89b925d51e890
kylo18 <carlossabijon04@Gmail.com>
2025-04-23 22:10:12 +0800

    Create Users.jsx

---
+++ caps-frontend/src/pages/Users.jsx
@@ -0,0 +1,12 @@
+import UserList from "../components/userList";
+
+const Users = () => {
+    
+    return (
+        <div className="">
+            <UserList/>
+        </div>
+    );
+};
+
+export default Users;
