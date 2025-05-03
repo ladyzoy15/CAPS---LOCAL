67c8b32da1b7eb7710146d8dc6909b1b70a54f24
kentrelatesahappenings <kentrelatesahappenings@gmail.com>
2025-05-03 15:00:37 +0800

    Update ProgramSeeder.php

--- Backend - Deployment/Modules/Users/Database/Seeders/ProgramSeeder.php
+++ Backend - Deployment/Modules/Users/Database/Seeders/ProgramSeeder.php
@@ -4,18 +4,24 @@ namespace Modules\Users\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Exception;
 
 class ProgramSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('programs')->insert([
-            ['programID' => 1, 'programName' => 'BS-CpE'],
-            ['programID' => 2, 'programName' => 'BS-EE'],
-            ['programID' => 3, 'programName' => 'BS-CE'],
-            ['programID' => 4, 'programName' => 'BS-ECE'],
-            ['programID' => 5, 'programName' => 'BS-ABE'],
-            ['programID' => 6, 'programName' => 'GE'],
-        ]);
+        try {
+            DB::table('programs')->insert([
+                ['programID' => 1, 'programName' => 'BS-CpE'],
+                ['programID' => 2, 'programName' => 'BS-EE'],
+                ['programID' => 3, 'programName' => 'BS-CE'],
+                ['programID' => 4, 'programName' => 'BS-ECE'],
+                ['programID' => 5, 'programName' => 'BS-ABE'],
+                ['programID' => 6, 'programName' => 'GE'],
+            ]);
+        } catch (Exception $e) {
+            Log::error('Failed to seed programs table: ' . $e->getMessage());
+        }
     }
 }
