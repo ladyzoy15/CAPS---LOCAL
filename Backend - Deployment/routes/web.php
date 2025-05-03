451243996de3887c1a8ed654a6a460e2dc73f3b3
kentrelatesahappenings <kentrelatesahappenings@gmail.com>
2025-05-03 15:03:37 +0800

    Update web.php

--- Backend - Deployment/routes/web.php
+++ Backend - Deployment/routes/web.php
@@ -2,6 +2,3 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Users\Controllers\AuthController;
-
-Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
