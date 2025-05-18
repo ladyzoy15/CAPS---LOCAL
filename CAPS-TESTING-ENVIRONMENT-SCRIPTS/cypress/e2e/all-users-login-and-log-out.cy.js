fb1206cd512e6473753494329a4fb9d4b060ef80
kylo18 <carlossabijon04@Gmail.com>
2025-05-18 23:01:48 +0800

    UPDATED CODE FOR LOGIN/LOGOUT
    -NEW UI = NEW AUTOMATION CODE

--- CAPS-TESTING-ENVIRONMENT-SCRIPTS/cypress/e2e/all-users-login-and-log-out.cy.js
+++ CAPS-TESTING-ENVIRONMENT-SCRIPTS/cypress/e2e/all-users-login-and-log-out.cy.js
@@ -1,46 +1,141 @@
 describe('User Login Automation (Updated Production Users)', () => {
   const users = [
-    // Dean Account
-    { id: '23-A-12345', password: '12345678', name: 'GILLERT BONGCAC (Dean - Dapitan)' },
+    // Dean Accounts
+  {
+    id: '23-A-12345',
+    password: '12345678',
+    name: 'Gillert Bongcac (Dean - Dapitan)',
+    email: 'bongcac@gmail.com',
+    campus: 'Dapitan',
+    position: 'Dean',
+  },
 
-    // Program Chair Accounts
-    { id: '23-A-12346', password: '12345678', name: 'TROY LASCO (Program Chair - BSCpE - Dapitan)' },
-    { id: '23-A-11111', password: '12345678', name: 'AGRI BIO (Program Chair - BSABE - Tampilisan)' },
-    { id: '23-A-22222', password: '12345678', name: 'CE ENGR (Program Chair - BSCE - Dapitan)' },
-    { id: '23-A-33333', password: '12345678', name: 'ECE ENG (Program Chair - BSECE - Dapitan)' },
-    { id: '23-A-55555', password: '12345678', name: 'EE EGR (Program Chair - BSEE - Dapitan)' },
+  // Program Chair Accounts
+  {
+    id: '23-A-12346',
+    password: '12345678',
+    name: 'Troy Lasco (Program Chair - BSCpE - Dapitan)',
+    email: 'Troy@gmail.com',
+    campus: 'Dapitan',
+    position: 'Program Chair',
+    program: 'BSCpE',
+  },
+  {
+    id: '23-A-11111',
+    password: '12345678',
+    name: 'Agri Bio (Program Chair - BSABE - Tampilisan)',
+    email: 'abe@gmail.com',
+    campus: 'Tampilisan',
+    position: 'Program Chair',
+    program: 'BSABE',
+  },
+  {
+    id: '23-A-22222',
+    password: '12345678',
+    name: 'CE Engr (Program Chair - BSCE - Dapitan)',
+    email: 'ce@gmail.com',
+    campus: 'Dapitan',
+    position: 'Program Chair',
+    program: 'BSCE',
+  },
+  {
+    id: '23-A-33333',
+    password: '12345678',
+    name: 'ECE Eng (Program Chair - BSECE - Dapitan)',
+    email: 'ece@gmail.com',
+    campus: 'Dapitan',
+    position: 'Program Chair',
+    program: 'BSECE',
+  },
+  {
+    id: '23-A-55555',
+    password: '12345678',
+    name: 'EE Egr (Program Chair - BSEE - Dapitan)',
+    email: 'ee@gmail.com',
+    campus: 'Dapitan',
+    position: 'Program Chair',
+    program: 'BSEE',
+  },
 
-    // Faculty Accounts
-    { id: '23-A-12347', password: '12345678', name: 'RYANN ELUMBA (Faculty - BSCpE - Dapitan)' },
-    { id: '23-A-11112', password: '12345678', name: 'ABE2 BIO (Faculty - BSABE - Tampilisan)' },
-    { id: '23-A-22223', password: '12345678', name: 'CE2 ENGR (Faculty - BSCE - Dapitan)' },
-    { id: '23-A-33334', password: '12345678', name: 'ECE2 ENG (Faculty - BSECE - Dapitan)' },
-    { id: '23-A-55556', password: '12345678', name: 'EE2 EGR (Faculty - BSEE - Dapitan)' },
-  ];
+  // Faculty Accounts
+  {
+    id: '23-A-12347',
+    password: '12345678',
+    name: 'Ryann Elumba (Faculty - BSCpE - Dapitan)',
+    email: 'Ryann@gmail.com',
+    campus: 'Dapitan',
+    position: 'Faculty',
+    program: 'BSCpE',
+  },
+  {
+    id: '23-A-11112',
+    password: '12345678',
+    name: 'Abe2 Bio (Faculty - BSABE - Tampilisan)',
+    email: 'abe2@gmail.com',
+    campus: 'Tampilisan',
+    position: 'Faculty',
+    program: 'BSABE',
+  },
+  {
+    id: '23-A-22223',
+    password: '12345678',
+    name: 'CE2 Engr (Faculty - BSCE - Dapitan)',
+    email: 'ce2@gmail.com',
+    campus: 'Dapitan',
+    position: 'Faculty',
+    program: 'BSCE',
+  },
+  {
+    id: '23-A-33334',
+    password: '12345678',
+    name: 'ECE2 Eng (Faculty - BSECE - Dapitan)',
+    email: 'ece2@gmail.com',
+    campus: 'Dapitan',
+    position: 'Faculty',
+    program: 'BSECE',
+  },
+  {
+    id: '23-A-55556',
+    password: '12345678',
+    name: 'EE2 Egr (Faculty - BSEE - Dapitan)',
+    email: 'ee2@gmail.com',
+    campus: 'Dapitan',
+    position: 'Faculty',
+    program: 'BSEE',
+  },
+];  
 
   users.forEach((user) => {
     it(`should log in and log out as ${user.name}`, () => {
+      cy.viewport(1280, 800);
+
       cy.visit('https://caps-test2.coeofjrmsu.com');
 
-      // Login
-      cy.get('input[placeholder="Enter ID Number"]').type(user.id);
-      cy.get('input[placeholder="Enter Password"]').type(user.password);
-      cy.get('button[type="submit"]').click();
+      cy.get('input#userCode').should('exist').first().type(user.id);
+      cy.get('input[type="password"]').should('exist').first().type(user.password);
+
+      // Debug: check how many buttons with type submit and text LOG IN are present
+      cy.get('button[type="submit"]')
+        .then((btns) => {
+          cy.log(`Found ${btns.length} submit buttons`);
+          const visibleBtns = Cypress.$(btns).filter(':visible');
+          cy.log(`Found ${visibleBtns.length} visible submit buttons`);
+        });
+
+      // Click login button forcibly to bypass visibility issues
+      cy.get('button[type="submit"]').contains('LOG IN').click({ force: true });
+
+      // Intercept profile API and wait
+      cy.intercept('GET', '/api/user/profile').as('getUserProfile');
+      cy.wait('@getUserProfile', { timeout: 10000 });
       cy.url().should('include', '-dashboard');
 
-      // Wait for user profile API to confirm login success
-      cy.intercept('GET', '/api/user/profile').as('getUserProfile');
-      cy.wait('@getUserProfile');
-
-      // Open the menu and log out
+      // Open logout menu
       cy.get('i.bx.bx-dots-vertical-rounded').click();
-      cy.get('button')
-        .contains('Log-out')
-        .should('be.visible', { timeout: 20000 })
-        .first()
-        .click();
 
-      // Confirm redirection to login
+      // Click logout button forcibly if needed
+      cy.get('button').contains('Log-out').click({ force: true });
+
       cy.url().should('not.include', '-dashboard');
     });
   });
