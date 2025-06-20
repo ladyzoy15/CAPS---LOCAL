f7500d647cd92d4a3174e57c5d5be5f0b9c0f26d
kentrelatesahappenings <kentrelatesahappenings@gmail.com>
2025-06-20 21:26:50 +0800

    Update DatabaseSeeder.php

--- Backend - Deployment/database/seeders/DatabaseSeeder.php
+++ Backend - Deployment/database/seeders/DatabaseSeeder.php
@@ -9,15 +9,15 @@ use Illuminate\Support\Facades\Log;
 
 require_once base_path('Modules/Users/Database/Seeders/RolesTableSeeder.php');
 require_once base_path('Modules/Users/Database/Seeders/CampusesTableSeeder.php');
-//require_once base_path('Modules/Users/Database/Seeders/UsersTableSeeder.php');
+require_once base_path('Modules/Users/Database/Seeders/UsersTableSeeder.php');
 require_once base_path('Modules/Users/Database/Seeders/ProgramSeeder.php');
-//require_once base_path('Modules/Subjects/Database/Seeders/SubjectsTableSeeder.php');
+require_once base_path('Modules/Subjects/Database/Seeders/SubjectsTableSeeder.php');
 require_once base_path('Modules/Subjects/Database/Seeders/YearLevelsTableSeeder.php');
 require_once base_path('Modules/Questions/Database/Seeders/CoverageSeeder.php');
 require_once base_path('Modules/Questions/Database/Seeders/DifficultySeeder.php');
 require_once base_path('Modules/Questions/Database/Seeders/PurposeSeeder.php');
 require_once base_path('Modules/Questions/Database/Seeders/StatusSeeder.php');
-//require_once base_path('Modules/Questions/Database/Seeders/QuestionsTableSeeder.php');
+require_once base_path('Modules/Questions/Database/Seeders/QuestionsTableSeeder.php');
 require_once base_path('Modules/Users/Database/Seeders/StudentsTableSeeder.php');
 require_once base_path('Modules/Users/Database/Seeders/SexesTableSeeder.php');
 
@@ -33,13 +33,13 @@ class DatabaseSeeder extends Seeder
             \Modules\Users\Database\Seeders\ProgramSeeder::class,
             \Modules\Questions\Database\Seeders\StatusSeeder::class,
             \Modules\Users\Database\Seeders\SexesTableSeeder::class,
-            //\Modules\Users\Database\Seeders\UsersTableSeeder::class,
+            \Modules\Users\Database\Seeders\UsersTableSeeder::class,
             \Modules\Subjects\Database\Seeders\YearLevelsTableSeeder::class,
-            //\Modules\Subjects\Database\Seeders\SubjectsTableSeeder::class,
+            \Modules\Subjects\Database\Seeders\SubjectsTableSeeder::class,
             \Modules\Questions\Database\Seeders\CoverageSeeder::class,
             \Modules\Questions\Database\Seeders\DifficultySeeder::class,
             \Modules\Questions\Database\Seeders\PurposeSeeder::class,
-            //\Modules\Questions\Database\Seeders\QuestionsTableSeeder::class,
+            \Modules\Questions\Database\Seeders\QuestionsTableSeeder::class,
             \Modules\Users\Database\Seeders\StudentsTableSeeder::class,
         ];
 
