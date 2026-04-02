53429c951554154816367fc0d2eb00516fd93144
kylo18 <carlossabijon04@Gmail.com>
2026-04-03 03:33:52 +0800

    Update forgotPassForm.jsx

--- Frontend - Deployment/src/components/forgotPassForm.jsx
+++ Frontend - Deployment/src/components/forgotPassForm.jsx
@@ -1,14 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-<<<<<<< Updated upstream
 import ForgotPasswordModal from "./ForgotPasswordModal";
-=======
-import univLogo from "../assets/univLogo.png";
-import AppVersion from "../components/appVersion";
-import collegeLogo from "/src/assets/college-logo.png";
-import Toast from "./Toast";
-import useToast from "../hooks/useToast";
->>>>>>> Stashed changes
 
 const ForgotPasswordForm = () => {
   const [isOpen, setIsOpen] = useState(true);
